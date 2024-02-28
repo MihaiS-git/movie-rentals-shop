23cbabd126998372e4c608ee9dbd8ffab3aebcfa
MihaiS-git <ingsuciumihai@gmail.com>
2024-02-28 13:04:45 +0200

    refactor auth feature and disable buttons in navbar

--- src/app/components/auth/auth.component.ts
+++ src/app/components/auth/auth.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { AuthResponseData, AuthService } from '../../services/auth.service';
 import { NgForm } from '@angular/forms';
 import { Observable } from 'rxjs';
@@ -9,18 +9,22 @@ import { Observable } from 'rxjs';
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.css'
 })
-export class AuthComponent {
-  isLoginMode = true;
+export class AuthComponent implements OnInit{
+  isLoginMode: boolean;
   isLoading = false;
   error: string = null;
 
-  constructor(private authService: AuthService, private router: Router) { }
-  onSwitchMode() {
-    this.isLoginMode = !this.isLoginMode;
+  constructor(private authService: AuthService, private router: Router, private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.route.url.subscribe(urlSegments => {
+      this.isLoginMode = urlSegments[0].path === 'signin';
+     });
   }
 
   onSubmit(form: NgForm) {
     if (!form.valid) {
+      alert(this.error);
       return;
     }
     const email = form.value.email;
